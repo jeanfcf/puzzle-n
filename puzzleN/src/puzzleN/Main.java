a6a20c2eaccfd39c929153ab2f4356d9ee2d3e47
jeean287 <49296442+jeean287@users.noreply.github.com>
2019-04-05 18:35:50 -0300

    Update Main.java

--- puzzleN/src/puzzleN/Main.java
+++ puzzleN/src/puzzleN/Main.java
@@ -2,13 +2,18 @@ package puzzleN;
 
 public class Main {
 	public static void main(String[] args) {
-
+		
+		//cria um novo jogador
 		Jogador jogador = new Jogador("jogador");
+		//cria um puzzle iniciando com o nome do jogador e a dificuldade facil
 		Puzzle teste = new Puzzle(jogador, Dificuldade.FACIL);
-
+		
+		//inicia a partida
 		teste.iniciaPartida();
+		//verifica se está ordenado
 		System.out.println(teste.getGridPuzzle().isTabuleiroOrdenado());
-
+		
+		//printa a matriz
 		for (int i = 0; i < teste.getGridPuzzle().getTamanhoMatriz(); i++) {
 			for (int j = 0; j < teste.getGridPuzzle().getTamanhoMatriz(); j++) {
 				System.out.print(teste.getGridPuzzle().getValorMatriz(i, j) + " ");
@@ -17,11 +22,14 @@ public class Main {
 				}
 			}
 		}
-
+		
+		//faz o movimento do 4 para direita na matriz
 		teste.getGridPuzzle().executaMovimento(1, 1, "direita");
-
+		
+		//printa uma linha para melhor visualização no console
 		System.out.println(" ");
-
+		
+		//printa a matriz com a nova posição do 4 e do 0
 		for (int i = 0; i < teste.getGridPuzzle().getTamanhoMatriz(); i++) {
 			for (int j = 0; j < teste.getGridPuzzle().getTamanhoMatriz(); j++) {
 				System.out.print(teste.getGridPuzzle().getValorMatriz(i, j) + " ");
@@ -30,11 +38,14 @@ public class Main {
 				}
 			}
 		}
-
+		
+		//muda a posição do 2 para baixo
 		teste.getGridPuzzle().executaMovimento(0, 1, "baixo");
-
+		
+		//printa uma linha para melhor visualização no console
 		System.out.println(" ");
-
+		
+		//printa a matriz com a nova posição do 2 e do 0
 		for (int i = 0; i < teste.getGridPuzzle().getTamanhoMatriz(); i++) {
 			for (int j = 0; j < teste.getGridPuzzle().getTamanhoMatriz(); j++) {
 				System.out.print(teste.getGridPuzzle().getValorMatriz(i, j) + " ");
@@ -43,11 +54,14 @@ public class Main {
 				}
 			}
 		}
-
+		
+		//muda a posição do 7 para esquerda
 		teste.getGridPuzzle().executaMovimento(0, 2, "esquerda");
-
+		
+		//printa uma linha para melhor visualização no console
 		System.out.println(" ");
-
+		
+		//printa a matriz com a nova posição do 7 e do 0
 		for (int i = 0; i < teste.getGridPuzzle().getTamanhoMatriz(); i++) {
 			for (int j = 0; j < teste.getGridPuzzle().getTamanhoMatriz(); j++) {
 				System.out.print(teste.getGridPuzzle().getValorMatriz(i, j) + " ");
